c38af40b2b076ebb8d61cfff6260ec7c68ad0f51
ajcmarce <a.j.c.marce@gmail.com>
2022-08-06 08:32:27 -0300

    testing stripe

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,11 +1,22 @@
 import Stripe from "stripe";
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
+
+    console.log(req.body);
+
     try {
       const params = {
+        submit_type: 'pay',
+        mode: 'payment',
+        payment_method_types: ['card'],
+        billing_address_collection: 'auto',
+        shipping_options: [
+          { shipping_rate: 'shr_1LTk6mFoQqjFDyasJmIg3bmb'},
+          { shipping_rate: 'shr_1LTkA5FoQqjFDyas7ZCKw4sD'}
+        ],
         line_items: [
           {
             // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
